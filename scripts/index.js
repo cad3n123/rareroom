90cdc835dc4c8079859292b37027a57a2d5dc28a
cad3n123 <cadencrowson@gmail.com>
2025-02-24 13:37:20 -0600

    Mobile rescaling

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,18 @@
+// Constant Variables
+const canvaBaseWidthPixel = 1366;
+const canvaBaseHeightPixel = 768;
+const canvaRatioPixel = 1366/768;
+const canvaBaseWidthPercent = 100;
+const canvaBaseHeightPercent = 56.2225;
+const canvaScale = 1.1;
+const canvaWidthPercent = canvaBaseWidthPercent * canvaScale;
+const canvaHeightPercent = canvaBaseHeightPercent * canvaScale;
+
 // Elements
 const [ $canvaContent ] = [ 'canva-content' ].map(id => document.getElementById(id));
 
 function main() {
-    setInterval(updataCanvaContentPosition, 100);
+    //setInterval(updataCanvaContentPosition, 100);
     updataCanvaContentPosition();
 }
 
@@ -10,10 +20,25 @@ function main() {
 function updataCanvaContentPosition() {
     let width = window.innerWidth;
     let height = window.innerHeight;
+
+    if (width / height > canvaRatioPixel) {
+        $canvaContent.style.width = `${canvaWidthPercent}%`;
+        $canvaContent.style.paddingTop = `${canvaHeightPercent}%`;
+    } else {
+        $canvaContent.style.width = `${100 * canvaRatioPixel / (width / height)}%`;
+        $canvaContent.style.paddingTop = `${100 * canvaRatioPixel / (width / height)}%`;
+
+    }
+    
+    // const windowRatioPixel = width / height;
+    // $canvaContent.style.width = `${canvaWidthPercent * canvaRatioPixel / windowRatioPixel}%`;
+    // $canvaContent.style.paddingTop = `${canvaHeightPercent * canvaRatioPixel / windowRatioPixel}%`;
+    
     console.log($canvaContent.offsetHeight);
     $canvaContent.style.left = ((width - $canvaContent.offsetWidth) / 2) + "px";
     $canvaContent.style.top = ((height - $canvaContent.offsetHeight) / 2) + "px";
 }
 
 // Event Listeners
-window.addEventListener('load', main);
+window.addEventListener('DOMContentLoaded', main);
+window.addEventListener('resize', updataCanvaContentPosition);
