2ea43b691bf255904d5af05a06254a0723c6bec7
cad3n123 <cadencrowson@gmail.com>
2025-02-24 17:55:56 -0600

    About page added

--- scripts/index.js
+++ scripts/index.js
@@ -7,12 +7,14 @@ const canvaBaseHeightPercent = 56.2225;
 const canvaScale = 1.1;
 const canvaWidthPercent = canvaBaseWidthPercent * canvaScale;
 const canvaHeightPercent = canvaBaseHeightPercent * canvaScale;
+const homeURL = 'https://www.canva.com/design/DAGgCFOLnQU/IghWNzf-q0ltWVUEDiCkvA/view?embed';
+const aboutURL = 'https://www.canva.com/design/DAGgDNNskO8/nrhCTo31fi1cd9J3DP1tqA/view?embed';
 
 // Elements
-const [ $canvaContent ] = [ 'canva-content' ].map(id => document.getElementById(id));
+const [ $canvaContent, $canvaIframeHome, $canvaIframeAbout, $content, $contactButton, $aboutButton, $homeLogo, ] = [ 'canva-content', 'canva-iframe-home', 'canva-iframe-about', 'content', 'contact-button', 'about-button', 'home-logo' ].map(id => document.getElementById(id));
+const [ [ $nav ], [ $aboutText ] ] = [ 'nav', ':scope > p' ].map(descriptor => $content.querySelectorAll(descriptor));
 
 function main() {
-    //setInterval(updataCanvaContentPosition, 100);
     updataCanvaContentPosition();
 }
 
@@ -30,15 +32,30 @@ function updataCanvaContentPosition() {
 
     }
     
-    // const windowRatioPixel = width / height;
-    // $canvaContent.style.width = `${canvaWidthPercent * canvaRatioPixel / windowRatioPixel}%`;
-    // $canvaContent.style.paddingTop = `${canvaHeightPercent * canvaRatioPixel / windowRatioPixel}%`;
-    
-    console.log($canvaContent.offsetHeight);
     $canvaContent.style.left = ((width - $canvaContent.offsetWidth) / 2) + "px";
     $canvaContent.style.top = ((height - $canvaContent.offsetHeight) / 2) + "px";
+}
+function setIframe(iframe) {
+    [$canvaIframeHome, $canvaIframeAbout].forEach(iframe => {
+        //iframe.style.display = 'none';
+        iframe.style.zIndex = '-2';
+    });
+    //iframe.style.display = 'block';
+    iframe.style.zIndex = '0';
 }
 
 // Event Listeners
 window.addEventListener('DOMContentLoaded', main);
 window.addEventListener('resize', updataCanvaContentPosition);
+$contactButton.addEventListener('click', () => { setIframe(contactURL); });
+$aboutButton.addEventListener('click', () => { 
+    $nav.style.display = 'none';
+    $aboutText.style.display = 'block';
+    console.log($aboutText);
+    setIframe($canvaIframeAbout); 
+});
+$homeLogo.addEventListener('click', () => { 
+    $nav.style.display = 'block';
+    $aboutText.style.display = 'none';
+    setIframe($canvaIframeHome); 
+});
